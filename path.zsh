b4c98965b69426cb2a753c4c8bb55ebd487cf85f
Josua P. Sianturi <josuapsianturi@gmail.com>
2023-01-10 13:43:12 +0700

    Add zsh aliases and path

---
+++ path.zsh
@@ -0,0 +1,13 @@
+export PATH="/usr/local/sbin:$PATH"
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+# export COMPOSER_MIRROR_PATH_REPOS=1
+# export GOPATH=$HOME/Golang
+# export GOROOT=/usr/local/opt/go/libexec
+# export PATH=$PATH:$GOPATH/bin
+# export PATH=$PATH:$GOROOT/bin
+# export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"
+# export LDFLAGS="-L/opt/homebrew/opt/openssl@3/lib"
+# export CPPFLAGS="-I/opt/homebrew/opt/openssl@3/include"
+# export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig"
+# export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+# export PUPPETEER_EXECUTABLE_PATH=`which chromium`
